3f6341fc9893b51f7f7fd307e8a161d33a709165
noload <vaibhavkamble023@gmail.com>
2024-01-10 11:18:04 +0530

    fetch xml file data using ajax

---
+++ AjaxGradedAssignment/AjaxFetchXMLData/script.js
@@ -0,0 +1,37 @@
+// Function to load XML file using AJAX
+function loadXMLDoc(filename) {
+  xhttp = new XMLHttpRequest();
+
+  xhttp.open("GET", filename, false);
+  xhttp.send();
+  return xhttp.responseXML;
+}
+
+// Function to display restaurant data
+function showRestaurants() {
+  const xmlDoc = loadXMLDoc("restaurant.xml");
+  const restaurants = xmlDoc.getElementsByTagName("restaurant");
+  const restaurantList = document.getElementById("restaurants");
+  restaurantList.innerHTML = "";
+
+  for (let i = 0; i < restaurants.length; i++) {
+    const name = restaurants[i].getAttribute("name");
+    const address = restaurants[i].getAttribute("address");
+    const type = restaurants[i].getAttribute("type");
+
+    const listItem = document.createElement("li");
+    listItem.textContent = `Name : ${name} || Address : ${address}`;
+
+    if (type === "sitdown") {
+      listItem.style.color = "blue"; // Change text color for sit down restaurants
+    } else if (type === "bar") {
+      listItem.style.color = "green"; // Change text color for bar restaurants
+    }
+
+    restaurantList.appendChild(listItem);
+  }
+}
+
+// Call the function to display restaurant data when the page loads
+const btn = document.getElementById("fetch");
+btn.addEventListener("click", showRestaurants);
